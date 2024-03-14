1892c6cc2aec2533c51c58bf87732f057da605b8
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-03-14 16:49:14 -0500

    Console Welcome Page

---
+++ Basic_C#_Projects/Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello, \n World!");
+        Console.ReadLine();
+        }
+    }
+
