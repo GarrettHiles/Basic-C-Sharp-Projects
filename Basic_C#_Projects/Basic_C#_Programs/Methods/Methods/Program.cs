e406a4be2c2ba82e37a209f6f06c2e1ce763836a
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-08 16:52:28 -0500

    Calling methods assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace math = new //gives the class a new variable
+            Trace(); //calls the class 
+
+            int answer1 = math.PlusSeven(9); //first method where it just adds 7
+            Console.WriteLine(answer1);
+            Console.ReadLine();
+
+            int answer2 = math.TimesTwo(68); //Second method multiplies by 2
+            Console.WriteLine(answer2);
+            Console.ReadLine();
+
+            int answer3 = math.MinusEight(14); //Third method subtracts by 14
+            Console.WriteLine(answer3);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/Methods/Methods/Trace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Trace //so it can be used everywhere
+    {
+        public int PlusSeven(int input) //needs a input and a number
+        {
+            return input + 7; //gives number, input in the main page
+        }
+
+        public int TimesTwo(int input)
+        {
+            return input * 2;
+        }
+
+        public int MinusEight(int input)
+        {
+            return input - 8;
+        }
+    }
+}
