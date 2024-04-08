8fdf3bd04cd5a2c351a1fa89d9cecdc261f3eda1
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-08 18:14:05 -0500

    Main method assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SameName math = new SameName();
+
+            int result1 = math.Operation(2); //prints the methods
+            Console.WriteLine(result1);
+            Console.ReadLine();
+
+            int result2 = math.Operation(86.83m); //knows which method to do based on input type like int, decimal, string.
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            int result3 = math.Operation("34");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/SameName.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class SameName
+    {
+        public int Operation(int num)
+        {
+            int answer = num + 29; // takes in number and adds 29
+            return answer;
+        }
+
+        public int Operation(decimal num) //takes in a decimal and divides by 4
+        {
+            int answer = (int)(num / 4); //gives back intger not another deciaml
+            return answer;
+        }
+
+
+        public int Operation(string str) //takes a string in
+        {
+            if (int.TryParse(str, out int num))  //changes string into number
+            {
+                int answer = num * 2;
+                return answer;
+            }
+            else //if a whole number was not entered returns 0
+            {
+                return 0;
+            }
+        }
+    }
+}
