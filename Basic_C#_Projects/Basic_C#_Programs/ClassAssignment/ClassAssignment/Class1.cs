6d966faaee2c148bba3946158824fb2345c5adda
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-09 17:35:26 -0500

    Class Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/ClassAssignment/ClassAssignment/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Class1
+    {
+        public void inHalf(int num, out int result) 
+        {
+            result = num / 2;
+            Console.WriteLine(result);
+        }
+
+        public void Upper(int num1, int num2, out int result)
+        {
+            result = num1 + num2;
+        }
+
+        public void Upper(int num1, int num2, int num3,  out int result) //overloaded method
+        {
+            result = num1 * num2 * num3;
+        }
+
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 div = new Class1();
+            Console.WriteLine("Enter a number to be divided by 2: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result;
+
+            div.inHalf(input, out result); //calls divide by 2 method
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            //overloaded method with out parameters
+            div.Upper(5, 5, out result);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            div.Upper(3, 3, 3, out result);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
