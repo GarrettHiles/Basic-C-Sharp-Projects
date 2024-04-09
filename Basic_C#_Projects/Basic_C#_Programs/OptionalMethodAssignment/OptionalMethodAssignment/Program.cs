e5d89d585bb8868ddb9fcb2be143121f3af8339c
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-08 19:21:44 -0500

    Method Assignment. Optional number.

---
+++ Basic_C#_Projects/Basic_C#_Programs/OptionalMethodAssignment/OptionalMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Way math = new Way();
+
+            Console.WriteLine("Enter a number: ");
+            int num1 = int.Parse(Console.ReadLine()); //gets first number
+            Console.WriteLine("Enter another number (not required): "); //2nd number not required
+            string input = Console.ReadLine();
+
+            int num2 = 0;
+            if (!string.IsNullOrEmpty(input)) //for if they don't enter in optional number
+            {
+                num2 = int.Parse(input);
+            }
+
+            int result = math.Math(num1, num2); //calls method
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/OptionalMethodAssignment/OptionalMethodAssignment/Way.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodAssignment
+{
+    class Way
+    {
+        public int Math(int num1, int num2 = 0) // num2 = 0 makes it optional
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+    }
+}
