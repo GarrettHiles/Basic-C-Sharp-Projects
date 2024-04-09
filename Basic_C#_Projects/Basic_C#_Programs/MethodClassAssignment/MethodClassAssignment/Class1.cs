88f598307b5e9cc9ed205e6bee090d705f7d3251
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-08 19:45:47 -0500

    Method Class Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+   public class Class1
+    {
+        public void Leroy(int num1, int num2)
+        {
+            num1 += 2;
+
+            Console.WriteLine(num2); //writes it to the screen.
+        }
+
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+
+            math.Leroy(40, 13);
+            Console.ReadLine();
+
+
+            math.Leroy(num2: 11, num1: 33);
+
+            Console.ReadLine();
+        }
+    }
+}
