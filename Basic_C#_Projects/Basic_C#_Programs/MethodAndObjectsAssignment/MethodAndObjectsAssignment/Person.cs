62310ea20934ba6f196b8e3f9dcd6bac5344a235
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-09 18:03:50 -0500

    Method and Object Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/MethodAndObjectsAssignment/MethodAndObjectsAssignment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectsAssignment
+{
+    class Person
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {FirsName} {LastName}");
+        }
+    }
+
+    class Employee : Person
+    {
+        public int id { get; set; }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/MethodAndObjectsAssignment/MethodAndObjectsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee //calling class and filling variables
+            {
+                FirsName = "Sample",
+                LastName = "Student"
+            };
+            worker.SayName(); //calls method
+            Console.ReadLine();
+        }
+    }
+}
