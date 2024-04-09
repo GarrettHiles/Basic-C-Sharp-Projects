d7836db8843a9039ccad3d1f19dc5c53351f9440
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-09 18:50:19 -0500

    Abstract Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/AbstractAssignment/AbstractAssignment/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    abstract class Person //The abstract class
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void SayName(); //abstract method
+    }
+
+    
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/AbstractAssignment/AbstractAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee //instantiate employee object
+            {
+                FirstName = "Sample", //fill the variables
+                LastName = "Student"
+            };
+            employee.SayName(); //calls method
+            Console.ReadLine();
+        }
+    }
+}
