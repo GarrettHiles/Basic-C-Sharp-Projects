14e9e0ba7ca7228d533ba97cb9a9a8b9a573c507
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-09 19:14:21 -0500

    Polymorphism Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/PolymorphismAssignment/AbstractAssignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Employee : Person , IQuittable //inherits from Person and the interface
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+        public void Quit() //implements the quit method
+        {
+            Console.WriteLine($"{FirstName} {LastName} has quit.");
+        }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/PolymorphismAssignment/AbstractAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee //instantiate employee object
+            {
+                FirstName = "Sample", //fill the variables
+                LastName = "Student"
+            };
+            employee.SayName(); //calls method
+            Console.ReadLine();
+
+            IQuittable quittableEmployee = employee; //polymorphism to create IQuittable type object
+            quittableEmployee.Quit(); //calls the quit method
+            Console.ReadLine();
+        }
+    }
+}
