fd51803b117c8810d9dccc8f4b3e9ff60e6c26b0
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-10 18:22:12 -0500

    Operator Overload Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public static bool operator ==(Employee worker1, Employee worker2)
+        {
+            return worker1.Id == worker2.Id;
+        }
+
+        public static bool operator !=(Employee worker1, Employee worker2)
+        {
+            return !(worker1 == worker2);
+        }
+
+
+
+
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker1 = new Employee
+            {
+                Id = 1,
+                FirstName = "Gary",
+                LastName = "Hiles"
+
+            };
+
+            Employee worker2 = new Employee
+            {
+                Id = 2,
+                FirstName = "Ryan",
+                LastName = "Rohe"
+            };
+
+            if (worker1 == worker2)
+            {
+                Console.WriteLine("The employees have the same Id.");
+            }
+            else
+            {
+                Console.WriteLine("The employees have different Ids.");
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
