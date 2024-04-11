001918cec16bcfa64d9a5803d69dac6269590424
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-11 18:23:59 -0500

    Parsing Enums assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/ParsingEnumsAssignment/ParsingEnumsAssignment/DaysofWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class DaysofWeek
+    {
+        public enum Week //public so it can be used
+        {
+            Sunday=1, //assigns int value
+            Monday=2,
+            Tuesday=3,
+            Wednesday=4,
+            Thursday=5,
+            Friday=6,
+            Saturday=7
+        }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day of the week:");
+
+            try
+            {
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine()); //sees if entered value matches any of the values in the enum
+                Console.WriteLine($"The day is {day}");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day in the week."); //if they don't enter in a matching input somewhere in the enum
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
