132a8ae4fcac318357452627664df7de86d2563f
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-11 18:53:15 -0500

    Structure Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount { get; set; } //makes amount a data type decimal variable
+        }
+
+        static void Main(string[] args)
+        {
+            Number a = new Number { Amount = 209.32m }; //amount assigned to a.Amount 
+            Console.WriteLine(a.Amount);
+            Console.ReadLine();
+        }
+
+    }
+}
