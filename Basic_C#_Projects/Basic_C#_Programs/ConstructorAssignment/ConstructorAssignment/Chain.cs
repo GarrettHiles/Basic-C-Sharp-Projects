9ce16c4392172b8b871dd39597128dbea6246e37
Garrett Hiles <146273814+GarrettHiles@users.noreply.github.com>
2024-04-22 18:50:38 -0500

    Constructor Assignment

---
+++ Basic_C#_Projects/Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Chain
+    {
+        public string Monkey { get; set; }
+
+        public Chain(string name)
+        {
+            Monkey = name;
+        }
+        public Chain() : this("Gorilla") { }
+    }
+}
---
+++ Basic_C#_Projects/Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Always = "Here ";
+            var any = "type of data type";
+
+            Chain obj = new Chain("Chimp");
+
+            Console.WriteLine("This plus " + obj.Monkey);
+            Console.ReadLine();
+        }
+    }
+}
